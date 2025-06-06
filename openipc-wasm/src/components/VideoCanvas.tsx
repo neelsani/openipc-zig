108b3abef6a80249ccd8576650d3e3161f665de1
Neel Sani <neel@neels.dev>
2025-06-06 02:51:06 +0000

    upf

--- openipc-wasm/src/components/VideoCanvas.tsx
+++ openipc-wasm/src/components/VideoCanvas.tsx
@@ -18,6 +18,7 @@ export const VideoCanvas = forwardRef<HTMLCanvasElement, VideoCanvasProps>(
         <div className="bg-black rounded-lg p-4">
           <canvas
             ref={ref}
+            id="videoCanvas"
             width={1920}
             height={1080}
             className="w-full h-auto max-h-96 border-2 border-gray-700 rounded"
