798606b6d1c47e4c9da655813bf11ed311c83dcf
Neel Sani <neel@neels.dev>
2025-06-06 01:03:28 +0000

    update headers

--- openipc-wasm/vite.config.ts
+++ openipc-wasm/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Resource-Policy': 'same-origin'
     },
   },
   assetsInclude: ['**/*.wasm'],
